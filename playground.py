2471db253049e197f1c9531aa2aad2302d30a831
Ilya Semenov <semenovio@gmail.com>
2024-10-08 02:25:34 +0300

    Added dialog summary  processing

--- playground.py
+++ playground.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, jsonify
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage, SystemMessage
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import MessagesPlaceholder
 
 from dotenv import load_dotenv
 
@@ -17,7 +17,11 @@ load_dotenv()
 openai_api_key = os.environ.get("OPENAI_API_KEY")
 
 
-def generate_response(system_prompt="You are an AI assistant of general purpose", user_prompt="Greet me, please!", llm_version="gpt-3.5-turbo", llm_temperature=30):
+def generate_response(system_prompt="You are an AI assistant of general purpose", 
+                      user_prompt="Greet me, please!", 
+                      chat_history="",
+                      llm_version="gpt-3.5-turbo", 
+                      llm_temperature=30):
     try:
         # Initialize the LLM with the given parameters
         llm = ChatOpenAI(
@@ -30,14 +34,9 @@ def generate_response(system_prompt="You are an AI assistant of general purpose"
         )
 
         # Create a ChatPromptTemplate object to format the prompt
-        prompt_template = ChatPromptTemplate.from_messages(
-            [
-                SystemMessage(content=system_prompt),
-                HumanMessage(content=user_prompt)
-            ]
-        )
-        # Use the prompt template to create a formatted prompt
-        formatted_prompt = prompt_template.format()
+        formatted_prompt = f"""System: {system_prompt} \nChat history: {chat_history}\n Human: {user_prompt}"""
+
+#        print(formatted_prompt)
 
         # Generate a response using the formatted prompt
         response = llm.generate([formatted_prompt])
@@ -53,7 +52,6 @@ def generate_response(system_prompt="You are an AI assistant of general purpose"
         return f"{llm_version}: Bad LLM response: something went wrong"
 
 
-
 @app.route('/')
 def index():
     system_prompt_content = ""
@@ -64,6 +62,7 @@ def index():
         system_prompt_content = "System prompt file not found."    
     return render_template('index.html', system_prompt_content=system_prompt_content)
 
+
 @app.route('/chat', methods=['POST'])
 def chat():
     system_prompt = request.json['system_prompt']
@@ -71,18 +70,37 @@ def chat():
     user_message = request.json['user_message']
     llm_version = request.json['llm_version']
     llm_temperature = request.json['llm_temperature']
+    chat_history = request.json['chat_history']
+    history_type = request.json['history_type']
+
+    if history_type == "summary" and 'gpt-' in user_prompt:
+        try:
+            history = user_prompt.split(': ')[2]
+            history = json.loads(history)
+            chat_history = history['summary']
+        except:
+            pass
 
     # Update user prompt with previous messages
     user_prompt += f"\nUser: {user_message}"
 
-    response = generate_response(system_prompt, user_prompt, llm_version, llm_temperature)
+#    print(f"Chat history_type: {history_type}\nChat history: {chat_history}")
+
+    response = generate_response(system_prompt, user_prompt, chat_history, llm_version, llm_temperature)
+
+#    print(f"LLM response : {response}")
+
+    if "gpt-3.5-turbo" in response:  # workaround for ChatGPT 3.5 incorrect JSON output
+        response = response.split(": ")[1]
+
     response = json.loads(response)
 
-    return jsonify({'text': response['text'], 'is_last_message': response['is_last_message']})
+    return response
+
 
 @app.route('/save', methods=['POST'])
 def save_dialog():
-    print(request.json)
+#    print(request.json)
     dialog = request.json['chat_history']
     with open('dialog.txt', 'w') as f:
         f.write(json.dumps(dialog))
